dc971dcd560ee07bff577b1d60bdc2628ed14110
Sujal Shrestha <sujalst10@gmail.com>
2026-02-11 15:45:18 +0545

    Routes and middlerware completed

---
+++ backend/src/routes/adminRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const adminController = require('../controllers/adminController');
+const { authenticate, authorizeRole } = require('../middleware/authMiddleware');
+
+// All routes here require the user to be an 'admin'
+router.use(authenticate, authorizeRole('admin'));
+
+router.get('/analytics', adminController.getCityStats);
+router.get('/heatmap', adminController.getHeatmapData);
+router.patch('/zones/:id/weight', adminController.updateZonePriority);
+router.delete('/issues/:id', adminController.deleteSpamIssue);
+
+module.exports = router;
